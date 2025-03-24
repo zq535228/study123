6a09ca55f1be09d97500b73540ac574397181226
bookmac <zq535228@qq.com>
2025-03-24 18:35:52 +0800

    使用blog.jianyandashu.com，去掉了config.js中的base参数。

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-  base: '/study123/',
+  base: '/',
 
   title: 'VuePress',
   description: 'My first VuePress Site',
