dbf52cf6bd136f67fdc5468ada9d0669092127eb
检验大叔 <45892397+zq535228@users.noreply.github.com>
2025-03-24 20:03:50 +0800

    Delete docs directory

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "/Users/Zhuanz/reps/study123/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "/Users/Zhuanz/reps/study123/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
